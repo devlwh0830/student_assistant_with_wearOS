35055e53aa8bfd3995ad261dc112921e0d12ba86
이원희 <story3798@naver.com>
2023-08-22 21:28:38 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'test.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Flutter Way',
+      home: Main()
+    );
+  }
+}
---
+++ lib/test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:rotary_scrollbar/rotary_scrollbar.dart';
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  final scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: RotaryScrollWrapper(
+        rotaryScrollbar: RotaryScrollbar(
+          controller: scrollController,
+        ),
+        child: ListView.builder(
+          controller: scrollController,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                bottom: 10,
+              ),
+              child: Container(
+                color: Colors.grey,
+                child: Center(
+                    child: Text(
+                        '워치 테스트 $index',
+                      style: TextStyle(color: Colors.white),
+                    )
+                ),
+              ),
+            );
+          },
+          itemCount: 15,
+        ),
+      ),
+    );
+  }
+}
